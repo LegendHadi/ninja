dce7cd11db7b34fb0c3c9573d7247e974fec33f6
Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-06-20 14:30:03 +0330

    Add quote2

---
+++ lib/quote2.dart
@@ -0,0 +1,11 @@
+class Quote {
+  late String text;
+  late String author;
+  Quote({required this.text,required this.author});
+}
+
+// List quotes = [
+//   'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//   'Ut enim ad minim veniam, quis nostrud exercitation',
+//   'ullamco laboris nisi ut aliquip ex ea ',
+// ];
